c95566a675b0b36492bcd88810556447109c2c4f
PolinaBasova <p.basova@g.nsu.ru>
2023-02-28 19:42:09 +0700

    Fixed 3-caic-pi.c

--- 3-calc-pi.c
+++ 3-calc-pi.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-double calc_pi(int N);
+double calc_pi(int Prec);
 
 int main() {
-    int N;
-    printf("Input a precision: N = ");
-    scanf("%d", &N);
-    printf("%.*f\n", N, calc_pi(N));
+    int Prec;
+    printf("Input a precision: Prec = ");
+    scanf("%d", &Prec);
+    printf("%.*f\n", Prec, calc_pi(Prec));
 }
 
-double calc_pi(int N) {
+double calc_pi(int Prec) {
     double SumUp = 0, SumDown=0;
     int n=0;
     do {
@@ -18,6 +18,6 @@ double calc_pi(int N) {
         SumUp = SumDown + (4/double(2*n-1)) ;
         n++;
         SumDown = SumUp - (4/double(2*n-1));
-    } while (  fabs(SumUp - SumDown) >= pow(0.1, N) );
+    } while (  fabs(SumUp - SumDown) >= pow(0.1, Prec) );
     return ((SumUp+SumDown)/2);
 }
