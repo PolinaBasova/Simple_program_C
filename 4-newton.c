24f07256c1192b7b18799aa6ae53581beae8afc4
PolinaBasova <p.basova@g.nsu.ru>
2023-02-28 23:27:52 +0700

    Create 4-newton.c

---
+++ 4-newton.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+double Func(double x){
+    return x*x;
+}
+
+double dFunc(double x){
+    double dx = pow(0.1, -100);
+    return ((Func(x+dx)-Func(x))/dx);
+}
+
+
+int main() {
+    double Max, Min;
+    printf("Input approximation Max = ");
+    
+    printf("Max = ");
+    
+    
+    
+}
