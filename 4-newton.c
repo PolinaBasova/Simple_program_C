8cb35fcb8a068a730ae56ec2224d24cba3e79877
PolinaBasova <p.basova@g.nsu.ru>
2023-03-11 16:08:51 +0700

    Finish 4-newton

--- 4-newton.c
+++ 4-newton.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-double dx = 0, x0 = 0, x = 0;
-
-double Func( double x0 ){
-    return (x0*x0-4*x0+3);
+double Func(double x0){
+    return (x0*x0 - 1);
 }
 
-double dFunc( double x0){
-    return ((Func(x0+dx)-Func(x0))/dx);
+double dFunc(double x0, double dx){
+    return ((Func(x0 + dx) - Func(x0))/dx);
 }
 
-double d2Func( double x0){
-    return ((dFunc(x0+dx)-dFunc(x0))/dx);
+double resolve_question(double x0, double Prec){
+    double dx = 1.e-5;
+    double x = x0 - (Func(x0)/dFunc(x0, dx));
+    while (fabs(x - x0) >= pow(0.1, Prec)){
+        x0 = x;
+        x = x0 - (Func(x0)/dFunc(x0, dx));
+        printf("%f\n", x);
+    }
+    return (x);
 }
 
 int main() {
-    dx = pow(0.1, 5);
-    double Max, Min;
+    double x0;
     int Prec;
-    printf("Input approximation: Max = ");
-    scanf("%lf", &Max);
-    printf("Min = ");
-    scanf("%lf", &Min);
+    printf("Input approximation: ");
+    scanf("%lf", &x0);
     printf("Input a precision: Prec = ");
     scanf("%d", &Prec);
-    if (Func(Max)*d2Func(Max) > 0){
-        x0=Max;
-    }
-    else{
-        x0=Min;
-    }
-    x=x0-(Func(x0)/dFunc(x0));
-    while (  fabs(x - x0) >= pow(0.1, Prec) ){
-        x0=x;
-        x=x0-(Func(x0)/dFunc(x0));
-    }
-    printf("%.*f\n", Prec, x);
+    printf("%.*f\n", Prec, resolve_question(x0, Prec));
 }
