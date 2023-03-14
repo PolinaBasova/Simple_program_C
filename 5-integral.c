9c84d898863bf12ec4eb0536bb8c7e3692ee8fcc
PolinaBasova <p.basova@g.nsu.ru>
2023-03-14 23:37:22 +0700

    Fixing 5-integral

--- 5-integral.c
+++ 5-integral.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 
-double dx = 0, x = 0;
-
-double Func( double x ){
-    return (-x*x+4);
+double Func(double x){
+    return (pow(x, 2));
 }
 
-double dFunc( double x){
+double dFunc(double x, double dx){
     return ((Func(x+dx)-Func(x))/dx);
 }
 
-double d2Func( double x){
-    return ((dFunc(x+dx)-dFunc(x))/dx);
+double d2Func(double x, double dx){
+    return ((dFunc(x+dx, dx)-dFunc(x, dx))/dx);
+}
+
+double error(double Up, double Down, int Num, double dx){
+    double x2 = Down;
+    double Max = 0;
+    int j;
+    for (j = 1; j <= Num; j++){
+        if (fabs(d2Func(x2, dx))>= Max){
+            Max = fabs(d2Func(x2, dx));
+        }
+        x2 += dx;
+    }
+    return (((Up-Down)*Max*dx*dx)/12);
+}
+
+double calc_integral(double Up, double Down, int Num, double dx){
+    double x1 = Down;
+    double Sum = 0;
+    int i;
+    for (i = 1; i <= Num; i++){
+        Sum += Func(x1)*dx;
+        x1 += dx;
+    }
+    Sum += ((Func(Up) + Func(Down))*dx)/2;
+    return(Sum);
 }
 
 int main() {
-    double Up, Down, Integral;
-    int N, i;
+    double Up, Down;
+    int Num;
     printf("Input limits: Up = ");
     scanf("%lf", &Up);
     printf("Down = ");
     scanf("%lf", &Down);
-    printf("Input a number of partitions: N = ");
-    scanf("%d", &N);
-    dx = (Up-Down)/N;
-    x=dx/2;
-    for (i=2; i<=N; i++){
-        x+=dx;
-        Integral+=Func(x)*dx;
-    }
-    printf("%lf", Integral);
+    printf("Input a number of partitions: Num = ");
+    scanf("%d", &Num);
+    double dx = (Up - Down)/Num;
+    printf("%.10lf\n", calc_integral(Up, Down, Num, dx));
+    printf("%.10lf\n", error(Up, Down, Num, dx));
 }
