cabea8baf5a0d0180eb24009b5539e01799cddfc
PolinaBasova <p.basova@g.nsu.ru>
2023-03-21 20:37:57 +0700

    Create 5-integral

--- 5-integral.c
+++ 5-integral.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
-double Func(double x){
+double Func1(double x){
     return (pow(x, 2));
 }
 
-double dFunc(double x, double dx){
-    return ((Func(x+dx)-Func(x))/dx);
+double Func2(double x){
+    return (pow(x, 3));
 }
 
-double d2Func(double x, double dx){
-    return ((dFunc(x+dx, dx)-dFunc(x, dx))/dx);
+double dFunc(double (*Funcion) (double), double x, double dx){
+    return ((Funcion(x+dx)-Funcion(x))/dx);
 }
 
-double error(double Up, double Down, int Num, double dx){
-    double x2 = Down;
+double d2Func(double (*Funcion) (double), double x, double dx){
+    return ((dFunc(Funcion, x+dx, dx)-dFunc(Funcion, x, dx))/dx);
+}
+
+/*
+Ищет максимум второй производной, вычисляет ошибку
+*/
+double calc_error(double (*Funcion) (double), double Up, double Down, int Num, double dx){
+    double len_of_interval = Up - Down;
     double Max = 0;
     int j;
     for (j = 1; j <= Num; j++){
-        if (fabs(d2Func(x2, dx))>= Max){
-            Max = fabs(d2Func(x2, dx));
+        if (fabs(d2Func(Funcion, Down, dx))>= Max){
+            Max = fabs(d2Func(Funcion, Down, dx));
         }
-        x2 += dx;
+        Down += dx;
     }
-    return (((Up-Down)*Max*dx*dx)/12);
+    return ((len_of_interval*Max*dx*dx)/12);
 }
 
-double calc_integral(double Up, double Down, int Num, double dx){
-    double x1 = Down;
-    double Sum = 0;
+double calc_integral(double (*Funcion) (double), double Up, double Down, int Num, double dx){
+    double Sum = ((Funcion(Up) + Funcion(Down))*dx)/2;
     int i;
     for (i = 1; i <= Num; i++){
-        Sum += Func(x1)*dx;
-        x1 += dx;
+        Sum += Funcion(Down)*dx;
+        Down += dx;
     }
-    Sum += ((Func(Up) + Func(Down))*dx)/2;
     return(Sum);
 }
 
@@ -48,6 +53,8 @@ int main() {
     printf("Input a number of partitions: Num = ");
     scanf("%d", &Num);
     double dx = (Up - Down)/Num;
-    printf("%.10lf\n", calc_integral(Up, Down, Num, dx));
-    printf("%.10lf\n", error(Up, Down, Num, dx));
+    double (*Funcion) (double);
+    Funcion = &Func1;
+    printf("%.10lf\n", calc_integral(Funcion, Up, Down, Num, dx));
+    printf("%.10lf\n", calc_error(Funcion, Up, Down, Num, dx));
 }
