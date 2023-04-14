f02a09038dae7bd249683d41e19439969e2866d9
PolinaBasova <p.basova@g.nsu.ru>
2023-04-15 00:44:48 +0700

    Update 9_matrix.c

--- 9_matrix.c
+++ 9_matrix.c
@@ -14,118 +14,101 @@ void fill_matrix(Matrix* point){
     }
 }
 
-void print_matrix(Matrix any_matrix){
-    Matrix *point;
-    point = &any_matrix;
+void print_matrix(Matrix* data){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            printf("%f\t", point->arr[i][j]);
+            printf("%f\t", data->arr[i][j]);
         }
         printf("\n");
     }
 }
 
-Matrix sum_matrices(Matrix* point_A, Matrix* point_B){
-    Matrix sum;
-    Matrix* point_sum;
-    point_sum = &sum;
+void sum_matrices(Matrix* sum_point, Matrix* point_A, Matrix* point_B){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            point_sum->arr[i][j] = (point_A->arr[i][j]) + (point_B->arr[i][j]);
+            sum_point->arr[i][j] = (point_A->arr[i][j]) + (point_B->arr[i][j]);
         }
     }
-    return (sum);
 }
 
-Matrix multiplication_matrices(Matrix* point_A, Matrix* point_B){
-    Matrix mult;
-    Matrix* point_mult;
-    point_mult = &mult;
+void multiplication_matrices(Matrix* mult_point, Matrix* point_A, Matrix* point_B){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            point_mult->arr[i][j] = 0;
+            mult_point->arr[i][j] = 0;
             for (int s = 0; s < 3; s++){
-                point_mult->arr[i][j] += (point_A->arr[i][s]) * (point_B->arr[s][j]);
+                mult_point->arr[i][j] += (point_A->arr[i][s]) * (point_B->arr[s][j]);
             }
         }
     }
-    return (mult);
 }
 
 float calc_det(Matrix* point){
     float det = 0;
     int num_el = 0;  // создаем массив для элементов алг. дополнений
     float *data = (float*)malloc(4 * sizeof(float));
-    for (int i = 0; i < 3; i++){
-        for (int s = 0; s < 3; s++){
-            for (int k = 1; k < 3; k++){
-                if (s!=i){
-                    *(data + num_el) = point->arr[s][k];
+    for (int line_det = 0; line_det < 3; line_det++){
+        for (int line_alg = 0; line_alg < 3; line_alg++){
+            for (int column_alg = 1; column_alg < 3; column_alg++){
+                if (line_alg!=line_det){
+                    *(data + num_el) = point->arr[line_alg][column_alg];
                     num_el++;
                 }
             }
         }
         num_el = 0;
-        det+=pow(-1, i)*(point->arr[i][0])*((*data) * (*(data+3)) - (*(data+1)) * (*(data+2)));
+        det+=pow(-1, line_det)*(point->arr[line_det][0])*((*data) * (*(data+3)) - (*(data+1)) * (*(data+2)));
     }
     free(data);
     return(det);
 }
 
-Matrix transpose_matrix(Matrix* point){
-    Matrix transp;
-    Matrix* point_transp;
-    point_transp = &transp;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            point_transp->arr[i][j] = point->arr[j][i];
+void transpose_matrix(Matrix* transp_point, Matrix* point){
+    for(int line = 0; line < 3; line++){
+        for(int column = 0; column < 3; column++){
+            transp_point->arr[line][column] = point->arr[column][line];
         }
     }
-    return(transp);
 }
 
-Matrix inverse_matrix(Matrix* point){
-    Matrix inv;
-    Matrix* point_inv;
-    point_inv = &inv;
+void inverse_matrix(Matrix* inv_point, Matrix* point){
     int num_el = 0;  // создаем массив для элементов алг. дополнений
     float *data = (float*)malloc(4 * sizeof(float));
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            for (int s = 0; s < 3; s++){
-                for (int k = 0; k < 3; k++){
-                    if ((s!=i)&&(k!=j)){
-                        *(data + num_el) = point->arr[s][k];
+    for (int line_inv = 0; line_inv < 3; line_inv++){
+        for (int column_inv = 0; column_inv < 3; column_inv++){
+            for (int line_alg = 0; line_alg < 3; line_alg++){
+                for (int column_alg = 0; column_alg < 3; column_alg++){
+                    if ((line_alg!=line_inv)&&(column_alg!=column_inv)){
+                        *(data + num_el) = point->arr[line_alg][column_alg];
                         num_el++;
                     }
                 }
             }
             num_el = 0;
-            point_inv->arr[i][j] = pow(-1, i+j)*((*data) * (*(data+3)) - (*(data+1)) * (*(data+2)))/calc_det(point);
+            inv_point->arr[line_inv][column_inv] = pow(-1, line_inv+column_inv)*((*data) * (*(data+3)) - (*(data+1)) * (*(data+2)))/calc_det(point);
         }
     }
     free(data);
-    return(transpose_matrix(point_inv));
+    Matrix transp;
+    transpose_matrix(&transp, inv_point);
 }
 
-void define_from_file(FILE* file, Matrix* point_read_matrix){
-    file = fopen("file.txt", "r");
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            fscanf(file, "%f", point_read_matrix->arr[i][j]);
+void define_from_file(char file_name[11], Matrix* read_matrix){
+    FILE* file = fopen(file_name, "r");
+    for (int line = 0; line < 3; line++){
+        for (int column = 0; column < 3; column++){
+            fscanf(file, "%f", &read_matrix->arr[line][column]);
         }
     }
     fclose(file);
 }
 
-Matrix write_to_file(FILE* file, Matrix W){
-    Matrix* point_read_matrix;
-    point_read_matrix = &W;
-    file = fopen("file.txt", "w");
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            fprintf(file, "%f", point_read_matrix->arr[i][j]);
+void write_to_file(char file_name[9], Matrix* write_matrix){
+    FILE* file = fopen(file_name, "w");
+    for (int line = 0; line < 3; line++){
+        for (int column = 0; column < 3; column++){
+            fprintf(file, "%f ", write_matrix->arr[line][column]);
         }
+        fprintf(file, "\n");
     }
     fclose(file);
 }
@@ -141,28 +124,33 @@ int main(){
         fill_matrix(&B);
     }
     else{
-        FILE* file;
-        define_from_file(file, &A);
+        define_from_file("file_A.txt", &A);
+        define_from_file("file_B.txt", &B);
     }
     
     printf("Matrix А: \n");
-    print_matrix(A);
+    print_matrix(&A);
     
     printf("Matrix B: \n");
-    print_matrix(B);
+    print_matrix(&B);
     
     printf("Matrix А+B: \n");
-    print_matrix(sum_matrices(&A, &B));
+    Matrix sum;
+    sum_matrices(&sum, &A, &B);
+    print_matrix(&sum);
     
     printf("Matrix А*B: \n");
-    print_matrix(multiplication_matrices(&A, &B));
+    Matrix mult;
+    multiplication_matrices(&mult, &A, &B);
+    print_matrix(&mult);
     
     printf("det A: %f \n", calc_det(&A));
     printf("det В: %f \n", calc_det(&B));
     
     printf("Inverse matrix А: \n");
-    print_matrix(inverse_matrix(&A));
+    Matrix inv;
+    inverse_matrix(&inv, &A);
+    print_matrix(&inv);
     
-    FILE* file_record;
-    write_to_file(file_record, inverse_matrix(&A)); //Например, записываем в файл обратную матрицу
+    write_to_file("file.txt", &inv); //Записываем в файл обратную матрицу
 }
