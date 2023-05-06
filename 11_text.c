6287418fa37a0c7b6137027e019afc99d6a8b42c
PolinaBasova <p.basova@g.nsu.ru>
2023-05-06 22:27:52 +0700

    Update 11_text.c

--- 11_text.c
+++ 11_text.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+void make_capital(char* buffer) {
+    int i = 0;
+    while (*(buffer + i) != '\0') {
+        if (*(buffer + i - 2) == '!' || *(buffer + i - 2) == '.' || *(buffer + i - 2) == '?') {
+            if (*(buffer + i) >= 'a' && *(buffer + i) <= 'z'){
+                *(buffer + i) = toupper(*(buffer + i));
+            }
+        }
+        i++;
+    }
+    if (*buffer >= 'a' && *buffer <= 'z'){
+        *buffer = toupper(*buffer);
+    }
+}
+
+void make_small(char* buffer) {
+    int i = 0;
+    while (*(buffer + i) != '\0') {
+        if (*(buffer + i - 2) != '!' && *(buffer + i - 2) != '.' && *(buffer + i - 2) != '?'){
+            if (*(buffer + i) >= 'A' && *(buffer + i) <= 'Z') {
+                *(buffer + i) = tolower(*(buffer + i));
+            }
+        }
+        i++;
+    }
+}
 
 void replace_symb(char* buffer) {
     int i = 0;
     while (*(buffer + i) != '\0') {
-        if (*(buffer + i) >= 65 && *(buffer + i) <= 90) {
-            if (*(buffer + i - 2) != 33 && *(buffer + i - 2) != 46 && *(buffer + i - 2) != 63){
-                *(buffer + i)+=32;
+        if (*(buffer + i - 2) != '!' && *(buffer + i - 2) != '.' && *(buffer + i - 2) != '?'){
+            if (*(buffer + i) >= 'A' && *(buffer + i) <= 'Z') {
+                *(buffer + i) = tolower(*(buffer + i));
+            }
+        }
+        else {
+            if (*(buffer + i) >= 'a' && *(buffer + i) <= 'z'){
+                *(buffer + i) = toupper(*(buffer + i));
             }
         }
         i++;
+    }
+    if (*buffer >= 'a' && *buffer <= 'z'){
+        *buffer = toupper(*buffer);
     }
 }
 
@@ -17,7 +53,23 @@ void delete_space(char* buffer) {
     int i = 0;
     int j = 0;
     while (*(buffer + i) != '\0') {
-        if ((*(buffer + i) == 32) && ((*(buffer + i + 1) == 32)||(*(buffer + i + 1) == 46)||(*(buffer + i + 1) == 44))){
+        if (*buffer == ' '){
+            j = 0;
+            while(*(buffer + j) != '\0') {
+                *(buffer + j) = *(buffer + j + 1);
+                j++;
+            }
+        }
+        if (*(buffer + i) == ' '){
+            while (*(buffer + i + 1) == ' ') {
+                j = i;
+                while(*(buffer + j) != '\0') {
+                    *(buffer + j) = *(buffer + j + 1);
+                    j++;
+                } 
+            }
+        }
+        if ((*(buffer + i) == ' ') && ((*(buffer + i + 1) == '.')||(*(buffer + i + 1) == ','))){
             j = i;
             while(*(buffer + j) != '\0') {
                 *(buffer + j) = *(buffer + j + 1);
@@ -33,31 +85,26 @@ void insert_space(char* buffer) {
     int j = 0;
     while (*(buffer + i) != '\0') {
         j = strlen(buffer) + 1;
-        if (((*(buffer + i) == 33)||(*(buffer + i) == 44)||(*(buffer + i) == 46)||(*(buffer + i) == 58)||(*(buffer + i) == 59)||(*(buffer + i) == 63)) && ((*(buffer + i + 1) != 32))) {
+        if (((*(buffer + i) == '!')||(*(buffer + i) == ',')||(*(buffer + i) == '.')||(*(buffer + i) == ':')||(*(buffer + i) == ';')||(*(buffer + i) == '?')) && ((*(buffer + i + 1) != ' '))) {
             while (j != i){
                 *(buffer + j + 1) = *(buffer + j);
                 j--;
             }
-            *(buffer + i + 1) = 32;
+            *(buffer + i + 1) = ' ';
         }
         i++;
     }
 }
 
-void correct_string(char* buffer) {
-    delete_space(buffer);
-    insert_space(buffer);
-    replace_symb(buffer);
-}
-
 int main() {
     FILE* fp = fopen("text.txt", "r");
     FILE* wp = fopen("correct_text.txt", "w");
-    char buffer[250];
-    while(fgets(buffer, 250, fp) != NULL){
+    char buffer[200];
+    while(fgets(buffer, 200, fp) != NULL){
         delete_space(buffer);
         insert_space(buffer);
-        replace_symb(buffer);
+        make_small(buffer);
+        make_capital(buffer);
         fputs(buffer, wp);
     }
     fclose(wp);
